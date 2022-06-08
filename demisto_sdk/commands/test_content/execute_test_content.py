2ce2a2efc1d971edc2d907648af9a305cfa82796
esharf <57587340+esharf@users.noreply.github.com>
2022-06-08 12:56:14 +0300

    first

--- demisto_sdk/commands/test_content/execute_test_content.py
+++ demisto_sdk/commands/test_content/execute_test_content.py
@@ -62,8 +62,8 @@ def execute_test_content(**kwargs):
     build_context = BuildContext(kwargs, logging_manager)
     use_retries_mechanism = kwargs.get('use_retries', False)
     threads_list = []
-    for server_ip, port in build_context.instances_ips.items():
-        tests_execution_instance = ServerContext(build_context, server_private_ip=server_ip, tunnel_port=port,
+    for server_ip in build_context.instances_ips:
+        tests_execution_instance = ServerContext(build_context, server_private_ip=server_ip,
                                                  use_retries_mechanism=use_retries_mechanism)
         threads_list.append(Thread(target=tests_execution_instance.execute_tests))
 
