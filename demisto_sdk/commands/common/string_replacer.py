f54a972811d90edb7a784db572ecf6d9acb5b374
dorschw <81086590+dorschw@users.noreply.github.com>
2022-08-02 16:32:57 +0300

    not static anymore

--- demisto_sdk/commands/common/string_replacer.py
+++ demisto_sdk/commands/common/string_replacer.py
@@ -1,18 +1,32 @@
 import re
+from pathlib import Path
+
+from demisto_sdk.commands.common.constants import MarketplaceVersions
+from demisto_sdk.commands.common.handlers import JSON_Handler
+
+json = JSON_Handler()
 
 STRING_REPLACER_PREFIX = 'STR$'
 
 
-class StringReplacer:
-    DICTIONARY = {'foo': 'bar'}
-    REPLACEMENTS = {re.escape(STRING_REPLACER_PREFIX + k): v for k, v in DICTIONARY.items()}
-    PATTERN = re.compile('|'.join(REPLACEMENTS.keys()))
+MARKETPLACE_TO_DICTIONARY_FILE = {
+    MarketplaceVersions.XSOAR: Path(__file__).absolute().parent / 'string_replacer_values_xsoar.json',
+    MarketplaceVersions.MarketplaceV2: Path(__file__).absolute().parent / 'string_replacer_values_marketplacev2.json'
+}
 
-    @staticmethod
-    def replace(text: str):
-        if not StringReplacer.DICTIONARY:
+
+class StringReplacer:
+    def __init__(self, marketplace: MarketplaceVersions):
+        with open(MARKETPLACE_TO_DICTIONARY_FILE[marketplace]) as f:
+            self.dictionary = json.load(f)
+
+        self._replacements = {re.escape(STRING_REPLACER_PREFIX + k): v for k, v in self.dictionary.items()}
+        self._pattern = re.compile('|'.join(self._replacements.keys()))
+
+    def replace(self, text: str):
+        if not self.dictionary:
             return text
 
-        return StringReplacer.PATTERN.sub(
-            lambda m: StringReplacer.REPLACEMENTS[re.escape(m.group(0))], text
+        return self._pattern.sub(
+            lambda m: self._replacements[re.escape(m.group(0))], text
         )
--- demisto_sdk/commands/common/tests/string_replacer_test.py
+++ demisto_sdk/commands/common/tests/string_replacer_test.py
@@ -2,43 +2,27 @@ import re
 
 import pytest
 
+from demisto_sdk.commands.common.constants import MarketplaceVersions
 from demisto_sdk.commands.common.string_replacer import (
-    STRING_REPLACER_PREFIX,
-    StringReplacer,
-)
+    STRING_REPLACER_PREFIX, StringReplacer)
 
 
-class StringReplacerContextManager:
+class StringReplacerMock(StringReplacer):
     def __init__(self, replacement_dict: dict[str, str]):
-        self.dict_ = replacement_dict
-
-    def __enter__(self):
-        self.last_dict = StringReplacer.DICTIONARY
-        self.mock(self.dict_)
-
-    def __exit__(self, *args):
-        self.mock(self.last_dict)
-        self.last_dict = StringReplacer.DICTIONARY
-
-    @staticmethod
-    def mock(dict_: dict[str, str]):
-        StringReplacer.DICTIONARY = dict_
-        StringReplacer.REPLACEMENTS = {
-            re.escape(STRING_REPLACER_PREFIX + k): v for k, v in dict_.items()
-        }
-        StringReplacer.PATTERN = re.compile(
-            '|'.join(StringReplacer.REPLACEMENTS.keys())
-        )
+        super().__init__(MarketplaceVersions.XSOAR)  # does not matter, as the dictionary is overwritten
+        self.dictionary = replacement_dict
+        self._replacements = {re.escape(STRING_REPLACER_PREFIX + k): v for k, v in self.dictionary.items()}
+        self._pattern = re.compile('|'.join(self._replacements.keys()))
 
 
 @pytest.mark.parametrize(
-    'text,replacements,expected',
+    'text,dictionary,expected',
     (
             (f'{STRING_REPLACER_PREFIX}hello world', {'hello': 'hey'}, 'hey world'),
             (f'{STRING_REPLACER_PREFIX}hello world', {'what\'s up': 'hey'}, f'{STRING_REPLACER_PREFIX}hello world'),
             (f'{STRING_REPLACER_PREFIX}hello world', {'hello': ''}, ' world'),
-            (f'hello world', {'hello': 'hey'}, 'hello world'),
-            (f'hello world', {}, 'hello world'),
+            ('hello world', {'hello': 'hey'}, 'hello world'),
+            ('hello world', {}, 'hello world'),
             (f'{STRING_REPLACER_PREFIX}hello world', {}, f'{STRING_REPLACER_PREFIX}hello world'),
             (f'hello {STRING_REPLACER_PREFIX}world', {'world': 'planet'}, 'hello planet'),
             (f'{STRING_REPLACER_PREFIX}hello {STRING_REPLACER_PREFIX}world', {'hello': 'hey',
@@ -46,17 +30,19 @@ class StringReplacerContextManager:
             # todo more complicated tests, multiple replacements, recursion
     )
 )
-def test_replace(text: str, replacements: dict, expected: str):
-    with StringReplacerContextManager(replacements):
-        assert StringReplacer.replace(text) == expected
+def test_replace(text: str, dictionary: dict, expected: str):
+    assert StringReplacerMock(dictionary).replace(text) == expected
 
 
-def test_default_dict_values_exclude_prefix():
-    for key in StringReplacer.DICTIONARY:
+@pytest.mark.parametrize('marketplace', MarketplaceVersions)
+def test_default_dict_values_exclude_prefix(marketplace: MarketplaceVersions):
+    replacer = StringReplacer(marketplace)
+    for key in replacer.dictionary:
         assert not key.startswith(STRING_REPLACER_PREFIX)
-        assert (re.escape(STRING_REPLACER_PREFIX + key) in StringReplacer.REPLACEMENTS)
+        assert (re.escape(STRING_REPLACER_PREFIX + key) in replacer._replacements)
 
 
-def test_default_replacement_values_include_prefix():
-    for key in StringReplacer.REPLACEMENTS:
+@pytest.mark.parametrize('marketplace', MarketplaceVersions)
+def test_default_replacement_values_include_prefix(marketplace: MarketplaceVersions):
+    for key in StringReplacer(marketplace)._replacements:
         assert key.startswith(re.escape(STRING_REPLACER_PREFIX))
