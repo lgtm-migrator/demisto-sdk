ba3aee8ffbfceb7b67e00c148d4f4be3aa909cbc
dorschw <81086590+dorschw@users.noreply.github.com>
2022-08-02 17:40:23 +0300

    more tests

--- demisto_sdk/commands/common/string_replacer.py
+++ demisto_sdk/commands/common/string_replacer.py
@@ -8,10 +8,10 @@ json = JSON_Handler()
 
 STRING_REPLACER_PREFIX = 'STR$'
 
-
+_PARENT_FOLDER = Path(__file__).absolute().parent
 MARKETPLACE_TO_DICTIONARY_FILE = {
-    MarketplaceVersions.XSOAR: Path(__file__).absolute().parent / 'string_replacer_values_xsoar.json',
-    MarketplaceVersions.MarketplaceV2: Path(__file__).absolute().parent / 'string_replacer_values_marketplacev2.json'
+    MarketplaceVersions.XSOAR: _PARENT_FOLDER / 'string_replacer_values_xsoar.json',
+    MarketplaceVersions.MarketplaceV2: _PARENT_FOLDER / 'string_replacer_values_marketplacev2.json'
 }
 
 
@@ -20,13 +20,19 @@ class StringReplacer:
         with open(MARKETPLACE_TO_DICTIONARY_FILE[marketplace]) as f:
             self.dictionary = json.load(f)
 
-        self._replacements = {re.escape(STRING_REPLACER_PREFIX + k): v for k, v in self.dictionary.items()}
+        self._replacements = {
+            re.escape(STRING_REPLACER_PREFIX + k if not k.startswith(STRING_REPLACER_PREFIX) else k): v
+            for k, v in self.dictionary.items()
+        }
         self._pattern = re.compile('|'.join(self._replacements.keys()))
 
     def replace(self, text: str):
         if not self.dictionary:
             return text
 
-        return self._pattern.sub(
-            lambda m: self._replacements[re.escape(m.group(0))], text
-        )
+        previous = None
+
+        while previous != text:
+            previous = text
+            text = self._pattern.sub(lambda m: self._replacements[re.escape(m.group(0))], previous)
+        return text
--- demisto_sdk/commands/common/tests/string_replacer_test.py
+++ demisto_sdk/commands/common/tests/string_replacer_test.py
@@ -11,8 +11,34 @@ class StringReplacerMock(StringReplacer):
     def __init__(self, replacement_dict: dict[str, str]):
         super().__init__(MarketplaceVersions.XSOAR)  # does not matter, as the dictionary is overwritten
         self.dictionary = replacement_dict
-        self._replacements = {re.escape(STRING_REPLACER_PREFIX + k): v for k, v in self.dictionary.items()}
+        self._replacements = {
+            re.escape(STRING_REPLACER_PREFIX + k if not k.startswith(STRING_REPLACER_PREFIX) else k): v
+            for k, v in self.dictionary.items()
+        }
         self._pattern = re.compile('|'.join(self._replacements.keys()))
+
+
+@pytest.mark.parametrize('marketplace', MarketplaceVersions)
+def test_sanity(marketplace: MarketplaceVersions):
+    """
+    Given   a marketplace value
+    When    calling replace on a string that includes the first replaceable value in the dictionary file
+    Then    make sure the replacer does replace the string
+    """
+    replacer = StringReplacer(marketplace)
+    key, value = next(iter(replacer.dictionary.items()))
+    assert replacer.replace(f'foo{STRING_REPLACER_PREFIX}{key}bar') == f'foo{value}bar'
+
+
+@pytest.mark.parametrize('marketplace', MarketplaceVersions)
+def test_dictionary_loading(marketplace: MarketplaceVersions):
+    """
+    Given   a marketplace value
+    When    calling replace on a string that includes the first replaceable value in the dictionary file
+    Then    make sure the dictionary is not empty
+    """
+    replacer = StringReplacer(marketplace)
+    assert replacer.dictionary
 
 
 @pytest.mark.parametrize(
@@ -27,7 +53,7 @@ class StringReplacerMock(StringReplacer):
             (f'hello {STRING_REPLACER_PREFIX}world', {'world': 'planet'}, 'hello planet'),
             (f'{STRING_REPLACER_PREFIX}hello {STRING_REPLACER_PREFIX}world', {'hello': 'hey',
                                                                               'world': 'planet'}, 'hey planet'),
-            # todo more complicated tests, multiple replacements, recursion
+            (f'{STRING_REPLACER_PREFIX}1', {'1': f'{STRING_REPLACER_PREFIX}2', f'{STRING_REPLACER_PREFIX}2': '3'}, '3')
     )
 )
 def test_replace(text: str, dictionary: dict, expected: str):
@@ -39,10 +65,14 @@ def test_default_dict_values_exclude_prefix(marketplace: MarketplaceVersions):
     replacer = StringReplacer(marketplace)
     for key in replacer.dictionary:
         assert not key.startswith(STRING_REPLACER_PREFIX)
-        assert (re.escape(STRING_REPLACER_PREFIX + key) in replacer._replacements)
+        assert not key.startswith(re.escape(STRING_REPLACER_PREFIX))
+
+        prefix_free_key = key.removeprefix(STRING_REPLACER_PREFIX)
+        assert (re.escape(STRING_REPLACER_PREFIX + prefix_free_key) in replacer._replacements)
 
 
 @pytest.mark.parametrize('marketplace', MarketplaceVersions)
 def test_default_replacement_values_include_prefix(marketplace: MarketplaceVersions):
     for key in StringReplacer(marketplace)._replacements:
         assert key.startswith(re.escape(STRING_REPLACER_PREFIX))
+        assert not key.startswith(2 * re.escape(STRING_REPLACER_PREFIX))  # avoid double prefix
